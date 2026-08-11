2714838c47832942a7a180811957102183d78c94
navinds25 <navinds25@gmail.com>
2026-08-11 14:17:53 +0530

    updates

--- lib/comic2pdf/src/lib.rs
+++ lib/comic2pdf/src/lib.rs
@@ -2,6 +2,10 @@ use std::fs;
 use std::io;
 use std::path;
 use unrar::Archive;
+use krilla::Document;
+use krilla::page::PageSettings;
+use krilla::image::Image;
+use krilla::geom::Size;
 
 fn check_extension(path: &path::PathBuf) -> bool {
     if path.is_dir() {
@@ -36,9 +40,13 @@ pub fn read_path<P: AsRef<path::Path>>(path: P) -> Result<Vec<path::PathBuf>, io
 
 pub fn read_cbr_file<P: AsRef<path::Path>>(path: P) -> Result<(), io::Error> {
     let path = path.as_ref();
-    //let output_dir = path::Path::new("/tmp/comics/output/");
+    let output = "/tmp/comics/output/";
     let output_dir = path::Path::new("");
+    std::env::set_current_dir(output)?; // TODO: Fix HACK
+    //let output_dir = path::Path::new("");
     fs::create_dir_all(output_dir)?;
+    let mut list_of_files: Vec<path::PathBuf> = [].to_vec();
+
     let mut archive = Archive::new(path)
         .open_for_processing()
         .map_err(|e| io::Error::new(io::ErrorKind::Other, e.to_string()))?;
@@ -53,16 +61,17 @@ pub fn read_cbr_file<P: AsRef<path::Path>>(path: P) -> Result<(), io::Error> {
             fs::create_dir_all(parent)?;
         }
         archive = if header.entry().is_file() {
+            list_of_files.push(header.entry().filename.clone());
             println!("{:?}", header.entry());
             header
                 .extract_to(output_dir)
-                .map_err(|e| io::Error::new(io::ErrorKind::Other, e.to_string()))?
+                .map_err(|e| io::Error::new(io::ErrorKind::Other, format!("could not extract file: {}", e.to_string())))?
         } else {
             // build the directory path straight from the header entry
-            let dir_name = header.entry().filename.clone();
-            println!("{:?}", dir_name);
+            //let dir_name = header.entry().filename.clone();
+            //println!("{:?}", dir_name);
 
-            fs::create_dir_all(dir_name)?;
+            //fs::create_dir_all(dir_name)?;
 
             header
                 .skip()
@@ -70,6 +79,37 @@ pub fn read_cbr_file<P: AsRef<path::Path>>(path: P) -> Result<(), io::Error> {
         }
     }
 
+    list_of_files.sort();
+    println!("{:?}", list_of_files);
+
+    let mut document = Document::new();
+
+    for file in list_of_files {
+        let image_data = fs::read(file)?;
+        let mut page = document.start_page();
+        let mut surface = page.surface();
+        let interpolate = false;
+        let image = match file.extension().and_then(|s| s.to_str()) {
+            Some("jpg") => Image::from_jpeg(image_data.into(), interpolate)?,
+            Some("png") => Image::from_png(image_data.into(), interpolate)?,
+            Some(ext) => {
+                println!("Other extension: {}", ext);
+                continue;
+            }
+            None => {
+                println!("No extension found");
+                continue;
+            }
+        };
+        let u32_image_size = image.size();
+        let image_size: (f32, f32) = (u32_image_size.0 as f32, u32_image_size.1 as f32);
+        let size = Size::from_wh(image_size.0, image_size.1)?;
+        surface.draw_image(image, size);
+        surface.finish();
+        page.finish();
+    }
+    let pdf_bytes = document.finish()?;
+    std::fs::write("output.pdf", &pdf_bytes)?;
     Ok(())
 }
 
