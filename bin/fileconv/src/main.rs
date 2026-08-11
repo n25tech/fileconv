a89e9287e5ca40554c02ce09d08eadeeb0fbf315
navinds25 <navinds25@gmail.com>
2026-08-11 17:02:04 +0530

    implemented cbr to pdf

--- bin/fileconv/src/main.rs
+++ bin/fileconv/src/main.rs
@@ -1,7 +1,7 @@
-use comic2pdf::{read_cbr_file};
+use comic2pdf::{cbr_to_pdf};
 
 fn main() {
     let file = "/tmp/comics/V for Vendetta (Complete)/V for Vendetta 01 (1988) (c2c) (theProletariat-DCP).cbr".to_string();
-    let out = read_cbr_file(file);
+    let out = cbr_to_pdf(file);
     println!("{:?}", out);
 }
--- lib/comic2pdf/src/lib.rs
+++ lib/comic2pdf/src/lib.rs
@@ -1,6 +1,8 @@
 use std::fs;
 use std::io;
+use std::error::Error;
 use std::path;
+use std::path::PathBuf;
 use unrar::Archive;
 use krilla::Document;
 use krilla::page::PageSettings;
@@ -38,14 +40,14 @@ pub fn read_path<P: AsRef<path::Path>>(path: P) -> Result<Vec<path::PathBuf>, io
     Ok(entries)
 }
 
-pub fn read_cbr_file<P: AsRef<path::Path>>(path: P) -> Result<(), io::Error> {
+fn read_cbr_file<P: AsRef<path::Path>>(path: P) -> Result<Vec<(PathBuf, Vec<u8>)>, Box<dyn Error>> {
     let path = path.as_ref();
-    let output = "/tmp/comics/output/";
-    let output_dir = path::Path::new("");
-    std::env::set_current_dir(output)?; // TODO: Fix HACK
+    //let output = "/tmp/comics/output/";
     //let output_dir = path::Path::new("");
-    fs::create_dir_all(output_dir)?;
-    let mut list_of_files: Vec<path::PathBuf> = [].to_vec();
+    //std::env::set_current_dir(output)?; // TODO: Fix HACK
+    ////let output_dir = path::Path::new("");
+    //fs::create_dir_all(output_dir)?;
+    let mut list_of_files = Vec::new();
 
     let mut archive = Archive::new(path)
         .open_for_processing()
@@ -55,24 +57,17 @@ pub fn read_cbr_file<P: AsRef<path::Path>>(path: P) -> Result<(), io::Error> {
         .read_header()
         .map_err(|e| io::Error::new(io::ErrorKind::Other, e.to_string()))?
     {
-        let entry_path = output_dir.join(header.entry().filename.clone());
-        if let Some(parent) = entry_path.parent() {
-            println!("{:?}", parent);
-            fs::create_dir_all(parent)?;
-        }
+        //let entry_path = output_dir.join(header.entry().filename.clone());
+        //if let Some(parent) = entry_path.parent() {
+        //    println!("{:?}", parent);
+        //    fs::create_dir_all(parent)?;
+        //}
         archive = if header.entry().is_file() {
-            list_of_files.push(header.entry().filename.clone());
-            println!("{:?}", header.entry());
-            header
-                .extract_to(output_dir)
-                .map_err(|e| io::Error::new(io::ErrorKind::Other, format!("could not extract file: {}", e.to_string())))?
+            let filename = header.entry().filename.clone();
+            let data = header.read()?;
+            list_of_files.push((filename, data.0));
+            data.1
         } else {
-            // build the directory path straight from the header entry
-            //let dir_name = header.entry().filename.clone();
-            //println!("{:?}", dir_name);
-
-            //fs::create_dir_all(dir_name)?;
-
             header
                 .skip()
                 .map_err(|e| io::Error::new(io::ErrorKind::Other, e.to_string()))?
@@ -80,15 +75,14 @@ pub fn read_cbr_file<P: AsRef<path::Path>>(path: P) -> Result<(), io::Error> {
     }
 
     list_of_files.sort();
-    println!("{:?}", list_of_files);
+    return Ok(list_of_files)
+}
 
+fn create_pdf_from_images(list_of_files: Vec<(PathBuf, Vec<u8>)>) -> Result<(), Box<dyn Error>> {
     let mut document = Document::new();
 
-    for file in list_of_files {
-        let image_data = fs::read(file)?;
-        let mut page = document.start_page();
-        let mut surface = page.surface();
-        let interpolate = false;
+    for (file, image_data) in list_of_files {
+        let interpolate = true;
         let image = match file.extension().and_then(|s| s.to_str()) {
             Some("jpg") => Image::from_jpeg(image_data.into(), interpolate)?,
             Some("png") => Image::from_png(image_data.into(), interpolate)?,
@@ -103,13 +97,21 @@ pub fn read_cbr_file<P: AsRef<path::Path>>(path: P) -> Result<(), io::Error> {
         };
         let u32_image_size = image.size();
         let image_size: (f32, f32) = (u32_image_size.0 as f32, u32_image_size.1 as f32);
-        let size = Size::from_wh(image_size.0, image_size.1)?;
+        let size = Size::from_wh(image_size.0, image_size.1).ok_or_else(|| io::Error::new(io::ErrorKind::Other, "invalid image dimensions"))?;
+        let mut page = document.start_page_with(PageSettings::from_wh(image_size.0, image_size.1).ok_or_else(|| io::Error::new(io::ErrorKind::Other, "invalid page dimensions"))?);
+        let mut surface = page.surface();
         surface.draw_image(image, size);
         surface.finish();
         page.finish();
     }
     let pdf_bytes = document.finish()?;
-    std::fs::write("output.pdf", &pdf_bytes)?;
+    std::fs::write("/tmp/output.pdf", &pdf_bytes)?;
+    Ok(())
+}
+
+pub fn cbr_to_pdf<P: AsRef<path::Path>>(file: P) -> Result<(), Box<dyn Error>> {
+    let list_of_files = read_cbr_file(file)?;
+    let result = create_pdf_from_images(list_of_files);
     Ok(())
 }
 
@@ -129,8 +131,7 @@ mod tests {
     #[test]
     fn check_cbr_file() {
         let file = "/tmp/comics/V for Vendetta (Complete)/V for Vendetta 01 (1988) (c2c) (theProletariat-DCP).cbr".to_string();
-        let output = read_cbr_file(file).unwrap();
+        let output = cbr_to_pdf(file).unwrap();
         println!("{:?}", output);
-        assert_eq!(1, 0);
     }
 }
